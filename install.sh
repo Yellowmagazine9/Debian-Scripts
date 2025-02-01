a6d5186610fe60468c902613d9454ec3a81745ca
Yellowmagazine9 <121526370+Yellowmagazine9@users.noreply.github.com>
2025-02-01 13:28:48 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,4 +9,13 @@ sudo apt install -y cherrytree
 #neovim
 sudo apt install -y neovim
 
+#Terminal multiplexers
+#Terminator
+sudo apt install -y terminator
+
+#tmux 
+#sudo apt install -y tmux
+
+
+
 
