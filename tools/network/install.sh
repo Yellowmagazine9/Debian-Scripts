d72f96d2f7c1fdd09868bfb477622a70452c40c0
Yellowmagazine9 <121526370+Yellowmagazine9@users.noreply.github.com>
2025-02-01 17:49:04 +0100

    Update install.sh

--- tools/network/install.sh
+++ tools/network/install.sh
@@ -1,1 +1,9 @@
-!#/bin/bash 
+!#/bin/bash
+
+#proxy chains configuration file 
+sudo apt install -y proxychains
+
+# curl and wget, download and upload files, data to a server.
+sudo apt install -y curl wget
+
+
